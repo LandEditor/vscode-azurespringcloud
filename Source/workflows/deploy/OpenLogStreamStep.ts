13bff8ce4b7135e1f960ca93fb445ff59ea6a8f6
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-22 10:36:29 +0200

    squash!

--- Source/workflows/deploy/OpenLogStreamStep.ts
+++ Source/workflows/deploy/OpenLogStreamStep.ts
@@ -8,22 +8,29 @@ import { localize } from "../../utils";
 import { IAppDeploymentWizardContext } from "./IAppDeploymentWizardContext";
 
 export class OpenLogStreamStep extends AzureWizardExecuteStep<IAppDeploymentWizardContext> {
+	public priority: number = 145;
+	private readonly deployment: EnhancedDeployment;
 
-    public priority: number = 145;
-    private readonly deployment: EnhancedDeployment;
+	constructor(deployment: EnhancedDeployment) {
+		super();
+		this.deployment = deployment;
+	}
 
-    constructor(deployment: EnhancedDeployment) {
-        super();
-        this.deployment = deployment;
-    }
+	public async execute(
+		context: IAppDeploymentWizardContext,
+		progress: Progress<{ message?: string; increment?: number }>
+	): Promise<void> {
+		const message: string = localize(
+			"openLogStream",
+			"Opening application log stream..."
+		);
+		progress.report({ message });
+		await this.deployment.latestInstance.then((i) =>
+			i.startStreamingLogs(context)
+		);
+	}
 
-    public async execute(context: IAppDeploymentWizardContext, progress: Progress<{ message?: string; increment?: number }>): Promise<void> {
-        const message: string = localize('openLogStream', 'Opening application log stream...');
-        progress.report({ message });
-        await this.deployment.latestInstance.then(i => i.startStreamingLogs(context));
-    }
-
-    public shouldExecute(_context: IAppDeploymentWizardContext): boolean {
-        return true;
-    }
+	public shouldExecute(_context: IAppDeploymentWizardContext): boolean {
+		return true;
+	}
 }
