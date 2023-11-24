2fc16adde190db689ea42dcd29d22dfbea5e5bd9
Nikola Hristov <nikola@nikolahristov.tech>
2023-11-24 06:09:38 +0200

    squash!

--- Source/workflows/updatesettings/scalesettings/IScaleSettingsUpdateWizardContext.ts
+++ Source/workflows/updatesettings/scalesettings/IScaleSettingsUpdateWizardContext.ts
@@ -1,9 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { IResourceGroupWizardContext } from '@microsoft/vscode-azext-azureutils';
+import { IResourceGroupWizardContext } from "@microsoft/vscode-azext-azureutils";
 import { IScaleSettings } from "../../../model";
 
-export interface IScaleSettingsUpdateWizardContext extends IResourceGroupWizardContext {
-    newSettings: IScaleSettings;
+export interface IScaleSettingsUpdateWizardContext
+	extends IResourceGroupWizardContext {
+	newSettings: IScaleSettings;
 }
